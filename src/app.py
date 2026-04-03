808c42ba078135815ae8894849964dd823f32b1c
Muskan <kumarimuskan4921@gmail.com>
2026-04-03 21:53:24 +0530

    add dropdown option for city and locality

--- src/app.py
+++ src/app.py
@@ -10,6 +10,26 @@ app = Flask(
 
 # Load trained pipeline (preprocessor + model)
 model = joblib.load("models/house_price_model.pkl")
+data = pd.read_excel("house_prices_v2.xlsx")
+
+# clean columns
+data.columns = data.columns.str.strip().str.lower()
+
+# remove nulls
+data = data.dropna(subset=["city", "locality"])
+
+# convert to string safely
+data["city"] = data["city"].astype(str).str.lower()
+data["locality"] = data["locality"].astype(str).str.lower()
+
+city_locality_map = {}
+
+for city in data["city"].unique():
+    city_locality_map[city] = list(
+        data[data["city"] == city]["locality"].unique()
+    )
+
+
 def format_price(price):
     if price >= 10000000:
         return f"{price/10000000:.2f} Cr"
@@ -25,24 +45,24 @@ def home():
     high = None              # ✅ ADD
 
     if request.method == "POST":
-        data = {
-            "city": request.form["city"].lower(),
-            "locality": request.form["locality"].lower(),
-            "zone_category": request.form["zone_category"].lower(),
-            "property_type": request.form["property_type"].lower(),
-            "area_sqft": float(request.form["area_sqft"]),
-            "bedrooms": int(request.form["bedrooms"]),
-            "bathrooms": int(request.form["bathrooms"]),
-            "balconies": int(request.form["balconies"]),
-            "parking": int(request.form["parking"]),
-            "floor_no": int(request.form["floor_no"]),
-            "total_floors": int(request.form["total_floors"]),
-            "age_of_property": int(request.form["age_of_property"]),
-            "distance_from_metro_km": float(request.form["distance_from_metro_km"]),
-            "furnishing_status": request.form["furnishing_status"].lower()
+        form_data = {
+        "city": request.form["city"].lower(),
+        "locality": request.form["locality"].lower(),
+        "zone_category": request.form["zone_category"].lower(),
+        "property_type": request.form["property_type"].lower(),
+        "area_sqft": float(request.form["area_sqft"]),
+        "bedrooms": int(request.form["bedrooms"]),
+        "bathrooms": int(request.form["bathrooms"]),
+        "balconies": int(request.form["balconies"]),
+        "parking": int(request.form["parking"]),
+        "floor_no": int(request.form["floor_no"]),
+        "total_floors": int(request.form["total_floors"]),
+        "age_of_property": int(request.form["age_of_property"]),
+        "distance_from_metro_km": float(request.form["distance_from_metro_km"]),
+        "furnishing_status": request.form["furnishing_status"].lower()
         }
 
-        df = pd.DataFrame([data])
+        df = pd.DataFrame([form_data])
         raw_price = int(model.predict(df)[0])
 
         formatted_price = format_price(raw_price)
@@ -53,7 +73,8 @@ def home():
         "index.html",
         price=formatted_price,
         low=low,
-        high=high
+        high=high,
+        city_locality_map=city_locality_map
     )
 
 
